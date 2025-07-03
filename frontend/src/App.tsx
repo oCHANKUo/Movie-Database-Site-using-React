ff67510f1c81c27f9316d94237d6bc8628d2e7df
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-07-03 22:45:18 +0530

    home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <MovieCard title="Movie1" release_date="2024" url="" />
-      <MovieCard title="Movie2" release_date="2024" url="" />
-      <MovieCard title="Movie3" release_date="2024" url="" />
+      <Home />
     </>
   );
 }
