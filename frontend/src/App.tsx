71024d09fc268d911c696f898a0376033d33f454
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-07-04 19:02:32 +0530

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,19 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
